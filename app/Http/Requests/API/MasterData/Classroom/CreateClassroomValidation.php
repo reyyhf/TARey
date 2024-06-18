919675f7bb0996863492fc7a96e5c2d31769494f
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-06-18 09:39:43 +0700

    Add regex validation to classroom

--- app/Http/Requests/API/MasterData/Classroom/CreateClassroomValidation.php
+++ app/Http/Requests/API/MasterData/Classroom/CreateClassroomValidation.php
@@ -14,9 +14,25 @@ class CreateClassroomValidation extends BaseRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:classrooms,name',
+            'name' => [
+                'required',
+                'unique:classrooms,name',
+                'regex:/^(X|XI|XII) (IPA|IPS) [1-9]\d*$/'
+            ],
             'majority' => 'required',
             'quota' => 'required|integer',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, mixed>
+     */
+    public function messages()
+    {
+        return [
+            'name.regex' => 'The name must follow the pattern "X IPA 1", "X IPA 2", "XI IPA 1", "XI IPA 2", "XII IPA 1", "XII IPA 2", and so on.'
         ];
     }
 }
