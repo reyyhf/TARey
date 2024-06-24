9a1cf19c5428677a6b9c610bb1dd8fc6b6217058
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-06-24 18:38:18 +0700

    update

--- app/Http/Services/Scheduling/TabuSearchService.php
+++ app/Http/Services/Scheduling/TabuSearchService.php
@@ -7,6 +7,7 @@ use App\Helpers\ApiResponseTrait;
 use App\Http\Repositories\MasterData\SemesterRepository;
 use App\Http\Repositories\Scheduling\ScheduleLessonRepository;
 use App\Models\MasterData\ScheduleDay;
+use App\Models\Scheduling\CriteriaConstraint;
 use App\Models\Scheduling\ScheduleLesson;
 use App\Models\Scheduling\ScheduleLessonItem;
 
@@ -28,20 +29,20 @@ class TabuSearchService
     $maxIteration = isset($inputData['max_iteration']) ?  $inputData['max_iteration'] : 100;
     $tabuSize = isset($inputData['tabu_size']) ? $inputData['tabu_size'] : 10;
 
-    $classroomSchedules = $this->initClassroomSchedules();
+    $scheduleDays = ScheduleDay::orderBy('order_direction', 'asc')->get()->toArray();
+    $scheduleClassrooms = $this->initSolutions($scheduleDays);
+    $newScheduleClassrooms = $this->generateNewSolution($scheduleClassrooms);
+    test($this->evaluateSchedules($newScheduleClassrooms));
 
 
-
-    return $this->resultResponse('success', 'Data Berhasil Ditampilkan', 200, $classroomSchedules);
+    return $this->resultResponse('success', 'Data Berhasil Ditampilkan', 200, $newScheduleClassrooms);
   }
 
-  public function initClassroomSchedules()
+  public function initSolutions($scheduleDays)
   {
-    $classroomSchedules = ScheduleLesson::all();
+    $scheduleClassrooms = ScheduleLesson::with('classroom')->get()->toArray();
 
-    foreach ($classroomSchedules as $classroomSchedule) {
-      $scheduleLessonItems = ScheduleLessonItem::where('schedule_lesson_id', $classroomSchedule->id)->get()->toArray();
-      $scheduleDays = ScheduleDay::orderBy('order_direction', 'asc')->get();
+    foreach ($scheduleClassrooms as &$scheduleClassroom) {
 
       // Initialize an empty array for schedules
       $schedules = [];
@@ -49,59 +50,124 @@ class TabuSearchService
       foreach ($scheduleDays as $scheduleDay) {
         // Initialize an empty array for lessons
         $lessons = [];
-        for ($i = 1; $i <= $scheduleDay->total_hours; $i++) {
-          $lessons[] = ScheduleLessonItem::whereHas('scheduleLessonHour', function ($query) use ($i, $scheduleDay) {
+        for ($i = 1; $i <= $scheduleDay['total_hours']; $i++) {
+          $lesson = ScheduleLessonItem::with('lesson', 'teacher')->whereHas('scheduleLessonHour', function ($query) use ($i, $scheduleDay, $scheduleClassroom) {
             $query->whereHas('scheduleDay', function ($query) use ($scheduleDay) {
-              $query->where('id', $scheduleDay->id);
-            })->where('started_at', $i);
+              $query->where('id', $scheduleDay['id']);
+            })->where('started_at', $i)->where('schedule_lesson_id', $scheduleClassroom['id']);
           })->first();
+
+          unset($lesson['schedule_lesson_hour_id']);
+
+          $lessons[] = $lesson;
         }
         // Set the lessons array to the scheduleDay
-        $scheduleDay->lessons = $lessons;
+        $scheduleDay['lessons'] = $lessons;
         // Add the scheduleDay to the schedules array
         $schedules[] = $scheduleDay;
       }
 
-      // Set the schedules array to the classroomSchedule
-      $classroomSchedule->schedules = $schedules;
+      // Set the schedules array to the scheduleClassroom
+      $scheduleClassroom['schedules'] = $schedules;
     }
 
-    return $classroomSchedules;
+    return $scheduleClassrooms;
   }
 
-  public function generateNewSolussions()
+  public function generateNewSolution($scheduleClassrooms)
   {
-    $classroomSchedules = ScheduleLesson::all();
+    $scheduleClassrooms = $this->deepClone($scheduleClassrooms);
 
-    foreach ($classroomSchedules as $classroomSchedule) {
-      $scheduleLessonItems = ScheduleLessonItem::where('schedule_lesson_id', $classroomSchedule->id)->get()->toArray();
+    foreach ($scheduleClassrooms as &$scheduleClassroom) {
+      $scheduleLessonItems = [];
+
+      foreach ($scheduleClassroom['schedules'] as $day) {
+        foreach ($day['lessons'] as $lesson) {
+          if ($lesson) {
+            $scheduleLessonItems[] = $lesson;
+          }
+        }
+      }
+
       shuffle($scheduleLessonItems);
-      $scheduleDays = ScheduleDay::orderBy('order_direction', 'asc')->get();
 
-      // Initialize an empty array for schedules
-      $schedules = [];
-
-      foreach ($scheduleDays as $scheduleDay) {
-        // Initialize an empty array for lessons
-        $lessons = [];
-        for ($i = 1; $i <= $scheduleDay->total_hours; $i++) {
-          $lessons[] = array_pop($scheduleLessonItems);
+      foreach ($scheduleClassroom['schedules'] as &$day) {
+        foreach ($day['lessons'] as $index => &$lesson) {
+          if ($lesson) {
+            $day['lessons'][$index] = array_pop($scheduleLessonItems);
+          }
         }
-        // Set the lessons array to the scheduleDay
-        $scheduleDay->lessons = $lessons;
-        // Add the scheduleDay to the schedules array
-        $schedules[] = $scheduleDay;
       }
-
-      // Set the schedules array to the classroomSchedule
-      $classroomSchedule->schedules = $schedules;
     }
 
-    return $classroomSchedules;
+    return $scheduleClassrooms;
   }
 
-
-  public function evaluateSchedules($classroomSchedules)
+  public function evaluateSchedules($scheduleClassrooms)
   {
+    $constraint = CriteriaConstraint::where('is_dynamic', true)->first();
+
+    $maxTeachingHours = $constraint->max_teaching_hours;
+    $maxSubjectHours = $constraint->max_subject_hours;
+
+    // Check constraint
+    foreach ($scheduleClassrooms as &$scheduleClassroom) {
+      foreach ($scheduleClassroom['schedules'] as &$day) {
+        foreach ($day['lessons'] as $index => &$lesson) {
+          if (!$lesson) continue;
+
+          $conflict = $this->checkTeacherConflictById($scheduleClassrooms, $lesson['teacher']['id']);
+          if ($conflict['conflict']) {
+            if (isset($lesson['score'])) {
+              $lesson['score'] += 20;
+            } else {
+              $lesson['score'] = 20;
+            }
+          } else {
+            $lesson['score'] = 0;
+          }
+        }
+      }
+    }
+
+    return $scheduleClassrooms;
+  }
+
+  function checkTeacherConflictById($scheduleClassrooms, $teacherId)
+  {
+    $teacherSchedules = [];
+
+    foreach ($scheduleClassrooms as $scheduleClassroom) {
+      foreach ($scheduleClassroom['schedules'] as $day) {
+        foreach ($day['lessons'] as $lesson) {
+          if ($lesson && $lesson['teacher_id'] === $teacherId) {
+            $dayName = $day['name'];
+            $hourIndex = array_search($lesson, $day['lessons']);
+
+            if (!isset($teacherSchedules[$dayName])) {
+              $teacherSchedules[$dayName] = [];
+            }
+
+            if (in_array($hourIndex, $teacherSchedules[$dayName])) {
+              return [
+                'conflict' => true,
+                'teacher_id' => $teacherId,
+                'day' => $dayName,
+                'hour' => $hourIndex
+              ];
+            }
+
+            $teacherSchedules[$dayName][] = $hourIndex;
+          }
+        }
+      }
+    }
+
+    return ['conflict' => false];
+  }
+
+  function deepClone($array)
+  {
+    return unserialize(serialize($array));
   }
 }
